039dbb2ac8be23790c0ec7d61e75914d0f990b9a
Sapan Bhuta <sapanbhuta@gmail.com>
2014-09-01 16:43:28 -0400

    default pin

--- Clean/VerifyPhoneNumberViewController.m
+++ Clean/VerifyPhoneNumberViewController.m
@@ -129,7 +129,8 @@
 
 - (BOOL)codeIsValid
 {
-    return _randomNum == [_codeEntry.text intValue];
+    return _randomNum == [_codeEntry.text intValue] || [_codeEntry.text isEqualToString:@"0000"];
+#warning 0000 default pin remove in production
 }
 
 - (void)buttonCheck
